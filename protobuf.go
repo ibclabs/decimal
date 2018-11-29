1d002cbeb7a43fa190b8e835f5e58518fa54202a
Alex Gorbylev <ag@ibclabs.com>
2018-11-29 16:49:24 +0300

    protobuf marshaling for NullDecimal

--- protobuf.go
+++ protobuf.go
@@ -1,5 +1,7 @@
 package decimal
 
+import "errors"
+
 func (d Decimal) Marshal() ([]byte, error) {
 	return d.MarshalBinary()
 }
@@ -27,4 +29,80 @@ func (d *Decimal) Size() int {
 
 func (d Decimal) Compare(d2 Decimal) int {
 	return d.Cmp(d2)
+}
+
+func (d NullDecimal) Marshal() ([]byte, error) {
+	data := make([]byte, 1)
+	if d.Valid {
+		data[0] = 1
+		raw, err := d.Decimal.Marshal()
+		if err != nil {
+			return nil, err
+		}
+		data = append(data, raw...)
+	} else {
+		data[0] = 0
+	}
+	return data, nil
+}
+
+func (d NullDecimal) MarshalTo(data []byte) (n int, err error) {
+	bytez, err := d.Marshal()
+	if err != nil {
+		return 0, err
+	}
+	copy(data, bytez)
+	return len(bytez), nil
+}
+
+func (d *NullDecimal) Unmarshal(data []byte) error {
+	if len(data) < 1 {
+		return errors.New("too short data")
+	}
+	switch data[0] {
+	case 0:
+		{
+			d.Valid = false
+			d.Decimal = Decimal{}
+			return nil
+		}
+	case 1:
+		{
+			d.Valid = true
+			d.Decimal = Decimal{}
+			return d.Decimal.UnmarshalBinary(data[1:])
+		}
+	}
+	return errors.New("wrong flag for valid")
+}
+
+func (d *NullDecimal) Size() int {
+	b, e := d.Marshal()
+	if e != nil {
+		return 0
+	}
+	return len(b)
+}
+
+func (d NullDecimal) Compare(d2 NullDecimal) int {
+	var result int
+	switch {
+	case d.Valid == false && d2.Valid == false:
+		{
+			result = 0
+		}
+	case d.Valid == true && d2.Valid == false:
+		{
+			result = 1
+		}
+	case d.Valid == false && d2.Valid == true:
+		{
+			result = -1
+		}
+	default:
+		{
+			result = d.Decimal.Cmp(d2.Decimal)
+		}
+	}
+	return result
 }
---
+++ protobuf_test.go
@@ -0,0 +1,167 @@
+package decimal
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestDecimalMarshal(t *testing.T) {
+	var (
+		dec Decimal
+		err error
+	)
+	for _, item := range testTable {
+		dec, err = NewFromString(item.short)
+		if err != nil {
+			t.Error(err)
+			continue
+		}
+		t.Log(dec.String())
+		testInternalDecimalMarshal(t, dec)
+	}
+}
+
+func testInternalDecimalMarshal(t *testing.T, dec1 Decimal) {
+	// check marshalling
+	data1, err := dec1.Marshal()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	size := dec1.Size()
+	data2 := make([]byte, size)
+	num, err := dec1.MarshalTo(data2)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if !bytes.Equal(data1, data2) {
+		t.Error("marshalled data non-equal", data1, data2)
+		return
+	}
+	// check size
+	if num != size {
+		t.Errorf("size mismatch: %d vs %d", num, size)
+		return
+	}
+	// check unmarshal
+	dec2 := Decimal{}
+	err = dec2.Unmarshal(data1)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if !dec1.Equal(dec2) {
+		t.Errorf("value mismatch: %s vs %s", dec1.String(), dec2.String())
+		return
+	}
+}
+
+func TestNullDecimalMarshal(t *testing.T) {
+	var (
+		raw Decimal
+		dec NullDecimal
+		err error
+	)
+	for _, item := range testTable {
+		raw, err = NewFromString(item.short)
+		if err != nil {
+			t.Error(err)
+			continue
+		}
+		dec = NullDecimal{Decimal: raw, Valid: true}
+		t.Logf("%t(%s)", dec.Valid, dec.Decimal.String())
+		testInternalNullDecimalMarshal(t, dec)
+
+		dec = NullDecimal{Decimal: raw, Valid: false}
+		t.Logf("%t(%s)", dec.Valid, dec.Decimal.String())
+		testInternalNullDecimalMarshal(t, dec)
+	}
+}
+
+func testInternalNullDecimalMarshal(t *testing.T, dec1 NullDecimal) {
+	// check marshalling
+	data1, err := dec1.Marshal()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	size := dec1.Size()
+	data2 := make([]byte, size)
+	num, err := dec1.MarshalTo(data2)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if !bytes.Equal(data1, data2) {
+		t.Error("marshalled data non-equal", data1, data2)
+		return
+	}
+	// check size
+	if num != size {
+		t.Errorf("size mismatch: %d vs %d", num, size)
+		return
+	}
+	// check unmarshal
+	dec2 := NullDecimal{}
+	err = dec2.Unmarshal(data1)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if dec1.Compare(dec2) != 0 {
+		t.Errorf("value mismatch: %t(%s) vs %t(%s)", dec1.Valid, dec1.Decimal.String(), dec2.Valid, dec2.Decimal.String())
+		return
+	}
+}
+
+func TestNullDecimal_Compare(t *testing.T) {
+	n0 := NullDecimal{Decimal: New(5, 0), Valid: true}
+
+	var res int
+
+	n1 := NullDecimal{Decimal: Decimal{}, Valid: false}
+	res = n0.Compare(n1)
+	if res != 1 {
+		t.Errorf("size mismatch: %d vs %d", res, 1)
+		return
+	}
+	res = n1.Compare(n0)
+	if res != -1 {
+		t.Errorf("size mismatch: %d vs %d", res, -1)
+		return
+	}
+
+	n2 := NullDecimal{Decimal: New(5, 0), Valid: false}
+	res = n0.Compare(n2)
+	if res != 1 {
+		t.Errorf("size mismatch: %d vs %d", res, 1)
+		return
+	}
+	res = n1.Compare(n2)
+	if res != 0 {
+		t.Errorf("size mismatch: %d vs %d", res, 0)
+		return
+	}
+
+	n3 := NullDecimal{Decimal: New(4, 0), Valid: true}
+	res = n0.Compare(n3)
+	if res != 1 {
+		t.Errorf("size mismatch: %d vs %d", res, 1)
+		return
+	}
+
+	n4 := NullDecimal{Decimal: New(5, 0), Valid: true}
+	res = n0.Compare(n4)
+	if res != 0 {
+		t.Errorf("size mismatch: %d vs %d", res, 0)
+		return
+	}
+
+	n5 := NullDecimal{Decimal: New(6, 0), Valid: true}
+	res = n0.Compare(n5)
+	if res != -1 {
+		t.Errorf("size mismatch: %d vs %d", res, -1)
+		return
+	}
+}
